9e72c06a9ca5999f10eca68f1ada2b27bee4d998
Bart van Kuik <bart@dutchvirtual.nl>
2023-03-08 15:16:30 +0100

    Removed storyboard

---
+++ LocationWikipediaApp/ViewControllers/LocationListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LocationListViewController.swift
+//  LocationWikipediaApp
+//
+//  Created by Bart van Kuik on 08/03/2023.
+//
+
+import UIKit
+
+class LocationListViewController: ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+    }
+}
